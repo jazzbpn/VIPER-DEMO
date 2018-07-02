0a5e16409a0b017a9b2a31b3da8521a852b8392b
bipin <jazzbpn@gmail.com>
2018-07-02 14:10:12 +0545

    GetDatasorece and set it to UITableView

--- VIPER-demo/app-modules/login-module/view/ViewController.swift
+++ VIPER-demo/app-modules/login-module/view/ViewController.swift
@@ -12,11 +12,17 @@ class ViewController: UIViewController {
 
     var presentor:ViewToPresenterProtocol?
     
-    @IBOutlet weak var uiLabel: UILabel!
+    @IBOutlet weak var uiTableView: UITableView!
+    var noticeArrayList:Array<NoticeModel> = Array()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         presentor?.startFetchingNotice()
+        
+        uiTableView.delegate = self
+        uiTableView.dataSource = self
+        
+        
     }
 
 }
@@ -24,15 +30,52 @@ class ViewController: UIViewController {
 extension ViewController:PresenterToViewProtocol{
     
     func showNotice(noticeArray: Array<NoticeModel>) {
-        uiLabel.text = noticeArray[0].filesource
+
+        self.noticeArrayList = noticeArray
+        self.uiTableView.reloadData()
+        
     }
     
     func showError() {
 
-        let alert = UIAlertController(title: "Alert", message: "Problem Fetching News", preferredStyle: UIAlertControllerStyle.alert)
+        let alert = UIAlertController(title: "Alert", message: "Problem Fetching Notice", preferredStyle: UIAlertControllerStyle.alert)
         alert.addAction(UIAlertAction(title: "Okay", style: UIAlertActionStyle.default, handler: nil))
         self.present(alert, animated: true, completion: nil)
         
     }
     
+    
 }
+
+extension ViewController:UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return noticeArrayList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PrototypeCell", for: indexPath) as! PrototypeCell
+        cell.id.text = noticeArrayList[indexPath.row].id
+        cell.title.text = noticeArrayList[indexPath.row].title
+        cell.brief.text = noticeArrayList[indexPath.row].brief
+        cell.file_source.text = noticeArrayList[indexPath.row].filesource
+
+        return cell
+    }
+    
+    
+    
+    
+}
+
+class PrototypeCell:UITableViewCell{
+    
+    @IBOutlet weak var id: UILabel!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var brief: UILabel!
+    @IBOutlet weak var file_source: UILabel!
+    
+}
+
+
+
